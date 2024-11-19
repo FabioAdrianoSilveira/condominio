e8022e12243de7de37d1203619fddbe43801ac1f
Fábio Adriano Silveira <fabioadrianosilveira456@gmail.com>
2024-11-19 16:12:04 -0300

    Adição das estruturas

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
+struct Morador
+{
+    string nome;
+    int idade;
+};
+
+struct Apartamento {
+    int numero;
+    int andar;
+    Morador moradores[2];
+};
+
+struct Bloco
+{
+    string nome;
+    int qtdAndares;
+    Apartamento apartamentos[3];
+};
+
+struct Condominio
+{
+    string endereco;
+    string nome;
+    Bloco blocos[3];
+};
+
+
 
 int main()
 {
-
     return 0;
 }
